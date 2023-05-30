239625029606c1a7c96924320efdce610ee6bde2
AsQ <danila.udodov.workmail@gmail.com>
2023-05-30 13:22:10 +0300

    first save

---
+++ README.md
@@ -0,0 +1,26 @@
+![Typing SVG](https://readme-typing-svg.herokuapp.com?color=%2336BCF7&lines=крестики-нолики)
+![Typing SVG](https://readme-typing-svg.herokuapp.com?color=%2336BCF7&lines=tic-tac-toe)
+
+# RU - Русский
+
+## Игра в крестики - нолики
+
+Игра на двоих. Крестики - нолики, игра в терминале
+
+### Как начать?
+
++ Скачиваем файл
++ Кидаем его в любую папку
+    + Открываем и внутри указываем команду для очистки терминала
+
+# EN - English
+
+## Tic - tac - toe game
+
+A game for two. Tic-tac-toe, a game in the terminal
+
+### How to get started?
+
++ Download the file
++ We throw it into any folder
+    + Open it and specify the command inside to clear the terminal
---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+
+# Напишите команду отчистки консоли
+clear = 'clear'
+
+# Создание площадки для игры
+board = list(range(1, 10))
+
+# Рисование площадки
+def draw_board(board):
+    os.system(clear)
+    print("-" * 13)
+    for i in range(3):
+        print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
+        print("-" * 13)
+
+# Ввод участника
+def take_input(player_token):
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token + "? ")
+        try:
+            player_answer = int(player_answer)
+        except ValueError:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+        if 1 <= player_answer <= 9:
+            if str(board[player_answer - 1]) not in "XO":
+                board[player_answer - 1] = player_token
+                valid = True
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
+
+# Высчитывание итога
+def check_win(board):
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+    return False
+
+# Основной loop
+def main(board):
+    global wins
+    os.system(clear)
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            take_input("X")
+        else:
+            take_input("O")
+        counter += 1
+
+        tmp = check_win(board)
+        if tmp:
+            wins = f"{tmp}, выиграл!"
+            win = True
+            break
+        if counter == 9:
+            wins = "Ничья!"
+            break
+    draw_board(board)
+
+
+# Запуск программы
+if __name__ == "__main__":
+    main(board)
+
+# Для красоты выхода из программы
+input(f"{wins}\nНажмите Enter для выхода!")
